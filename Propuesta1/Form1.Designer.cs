2abb5ec1a7a717364d14af66c914f2111fca1b89
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2019-08-12 21:01:36 -0500

    Add project files.

---
+++ Propuesta1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Propuesta1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridClient = new System.Windows.Forms.DataGridView();
+            this.EditTextName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridClient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridClient
+            // 
+            this.dataGridClient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridClient.Location = new System.Drawing.Point(12, 103);
+            this.dataGridClient.Name = "dataGridClient";
+            this.dataGridClient.RowHeadersWidth = 51;
+            this.dataGridClient.RowTemplate.Height = 24;
+            this.dataGridClient.Size = new System.Drawing.Size(776, 280);
+            this.dataGridClient.TabIndex = 0;
+            // 
+            // EditTextName
+            // 
+            this.EditTextName.Location = new System.Drawing.Point(257, 68);
+            this.EditTextName.Name = "EditTextName";
+            this.EditTextName.Size = new System.Drawing.Size(503, 22);
+            this.EditTextName.TabIndex = 1;
+            this.EditTextName.TextChanged += new System.EventHandler(this.EditTextName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(251, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Listado de Clientes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(6, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre del cliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Location = new System.Drawing.Point(437, 410);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(218, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total de Clientes";
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.labelTotal.Location = new System.Drawing.Point(668, 410);
+            this.labelTotal.MinimumSize = new System.Drawing.Size(120, 0);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(120, 33);
+            this.labelTotal.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EditTextName);
+            this.Controls.Add(this.dataGridClient);
+            this.Name = "Form1";
+            this.Text = "Sistema de Negocios";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridClient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridClient;
+        private System.Windows.Forms.TextBox EditTextName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelTotal;
+    }
+}
+
---
+++ Propuesta1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Propuesta1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["frank"].ConnectionString);
+
+        public void ClientList()
+        {
+            using (SqlDataAdapter adapter = new SqlDataAdapter("Usp_ListaCLientes_Neptuno", connection))
+            {
+                adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataSet dataSet = new DataSet())
+                {
+                    adapter.Fill(dataSet, "Clientes");
+                    dataGridClient.DataSource = dataSet.Tables["Clientes"];
+                    labelTotal.Text = dataSet.Tables["Clientes"].Rows.Count.ToString();
+                }
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ClientList();
+        }
+
+        private void EditTextName_TextChanged(object sender, EventArgs e)
+        {
+            BindingSource bs = new BindingSource();
+            bs.DataSource = dataGridClient.DataSource;
+            bs.Filter = dataGridClient.Columns[2].HeaderText.ToString() + " LIKE '%" + EditTextName.Text + "%' ";
+            dataGridClient.DataSource = bs;
+        }
+    }
+}
